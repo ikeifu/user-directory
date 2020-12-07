2a5845590dc92d55ac84ee252d45c0898b55d904
Keith Fung <fungsenkeith@gmail.com>
2020-12-07 16:34:00 -0500

    sorting

--- src/components/Mainbody.js
+++ src/components/Mainbody.js
@@ -1,16 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import CardBody from "./Cardbody";
 function MainBody() {
-  function orderName() {}
+  const [sortState, setSortState] = useState({
+    employees: [],
+    search: "",
+    sortBy: "",
+    asc: true,
+  });
+  const sortList = (input) => {
+    console.log("poggers");
+    if (sortState.sortBy === input) {
+      let sort;
+      if (sortState.asc) {
+        sort = sortState.employees.sort((a, b) =>
+          a[input] < b[input] ? 1 : -1
+        );
+      } else {
+        sort = sortState.employees.sort((a, b) =>
+          a[input] > b[input] ? 1 : -1
+        );
+      }
+      setSortState({
+        ...sortState,
+        employees: sort,
+        sortBy: input,
+        asc: !sortState.asc,
+      });
+    } else {
+      setSortState({
+        ...sortState,
+        employees: sortState.employees.sort((a, b) =>
+          a[input] > b[input] ? 1 : -1
+        ),
+        sortBy: input,
+        asc: !sortState.asc,
+      });
+    }
+  };
   return (
     <table className="table">
       <thead>
         <tr>
-          <th scope="col" onClick={orderName}>
+          <th scope="col" onClick={sortList}>
             Name
           </th>
-          <th scope="col">Role</th>
-          <th scope="col">Department</th>
+          <th scope="col" onClick={sortList}>
+            Role
+          </th>
+          <th scope="col" onClick={sortList}>
+            Department
+          </th>
         </tr>
       </thead>
       <CardBody />
