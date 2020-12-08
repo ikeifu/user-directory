ba92cb72f0bed1981a7557406fd787f59caded02
Keith Fung <fungsenkeith@gmail.com>
2020-12-08 09:48:47 -0500

    removed global

--- src/util/GlobalContent.js
+++
@@ -1,9 +0,0 @@
-// import React {useState} from "react";
-// const [sortState, setSortState] = useState({
-//     employees: [],
-//     search: "",
-//     sortBy: "",
-//     asc: true,
-//   });
-
-//   export default Global
